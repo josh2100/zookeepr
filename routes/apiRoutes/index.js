15da09d5b48ecb93c3deadf4f127dce411a95906
joshgit <jschermann91@gmail.com>
2021-12-15 15:43:44 -0600

    zookeeper routes added

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 const animalRoutes = require("../apiRoutes/animalRoutes");
+/// self actualization
+const zookeeperRoutes = require("../apiRoutes/zookeeperRoutes");
 
 router.use(animalRoutes);
+
+router.use(zookeeperRoutes);
 
 module.exports = router;
