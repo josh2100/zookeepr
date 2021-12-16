6575ef0f337c4da5a68958d40bac3a790f3f5b3b
joshgit <jschermann91@gmail.com>
2021-12-16 14:35:27 -0600

    zookeeper search

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,10 +1,9 @@
 const router = require("express").Router();
 const animalRoutes = require("../apiRoutes/animalRoutes");
-/// self actualization
+/// changed from recommended
 const zookeeperRoutes = require("../apiRoutes/zookeeperRoutes");
+router.use(zookeeperRoutes);
 
 router.use(animalRoutes);
-
-router.use(zookeeperRoutes);
 
 module.exports = router;
